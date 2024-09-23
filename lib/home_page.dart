2387d03d43fad2117caf4989c12455f16473c7d4
Emerson Cavalcanti <emerson.cavalcanti@escolatecnicalimoeiro.com.br>
2024-09-23 20:23:20 -0300

    add listview

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,6 +8,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final TextEditingController _controller = TextEditingController();
+  final List<String> _tarefas = [];
+
+  void _addTarefa() {
+    if (_controller.text.isNotEmpty) {
+      setState(() {
+        _tarefas.add(_controller.text);
+        _controller.clear();
+      });
+    }
+  }
+
+  void _removerTarefa(int index) {
+    setState(() {
+      _tarefas.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,34 +34,52 @@ class _HomePageState extends State<HomePage> {
         foregroundColor: Colors.white,
         title: const Text('Lista de tarefas'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: [
-            const TextField(
-              decoration: InputDecoration(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              controller: _controller,
+              decoration: const InputDecoration(
                   labelText: 'Digite uma tarefa', border: OutlineInputBorder()),
             ),
-            const SizedBox(height: 10),
-            InkWell(
-              onTap: () {},
-              child: Container(
-                width: double.infinity,
-                height: 40,
-                decoration: const BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                ),
-                child: const Center(
-                    child: Text(
-                  'Adicionar',
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.bold),
-                )),
+          ),
+          const SizedBox(height: 10),
+          InkWell(
+            onTap: _addTarefa,
+            child: Container(
+              width: double.infinity,
+              height: 40,
+              decoration: const BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.all(Radius.circular(10)),
               ),
-            )
-          ],
-        ),
+              child: const Center(
+                  child: Text(
+                'Adicionar',
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              )),
+            ),
+          ),
+          Expanded(
+              child: ListView.builder(
+                  itemCount: _tarefas.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: const CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            'https://startbootstrap.github.io/startbootstrap-one-page-wonder/assets/img/01.jpg'),
+                      ),
+                      title: Text(_tarefas[index]),
+                      subtitle: const Text('Clique na lixeira para remover'),
+                      trailing: IconButton(
+                        onPressed: () => _removerTarefa(index),
+                        icon: const Icon(Icons.delete),
+                      ),
+                    );
+                  })),
+        ],
       ),
     );
   }
